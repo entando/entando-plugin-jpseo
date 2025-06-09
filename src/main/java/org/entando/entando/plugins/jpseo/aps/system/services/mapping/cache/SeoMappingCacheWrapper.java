2a342e84d8af6f170c4675be2faf4e9eb4a50bd1
Matteo E. Minnai <m.minnai@entando.com>
2025-06-09 14:45:33 +0200

    ENG-5542 code cleaning

--- src/main/java/org/entando/entando/plugins/jpseo/aps/system/services/mapping/cache/SeoMappingCacheWrapper.java
+++ src/main/java/org/entando/entando/plugins/jpseo/aps/system/services/mapping/cache/SeoMappingCacheWrapper.java
@@ -26,19 +26,19 @@ import com.agiletec.aps.system.services.page.IPage;
 import com.agiletec.aps.system.services.page.IPageManager;
 import com.agiletec.aps.system.services.page.PageMetadata;
 import com.agiletec.aps.util.ApsProperties;
-import org.entando.entando.aps.system.services.cache.IFCacheWithPipeline;
-import org.entando.entando.ent.exception.EntException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
+import org.entando.entando.aps.system.services.cache.IFCacheWithPipeline;
+import org.entando.entando.ent.exception.EntException;
+import org.entando.entando.ent.util.EntLogging.EntLogFactory;
+import org.entando.entando.ent.util.EntLogging.EntLogger;
 import org.entando.entando.plugins.jpseo.aps.system.services.mapping.ContentFriendlyCode;
 import org.entando.entando.plugins.jpseo.aps.system.services.mapping.FriendlyCodeVO;
 import org.entando.entando.plugins.jpseo.aps.system.services.mapping.ISeoMappingDAO;
-import org.entando.entando.ent.util.EntLogging.EntLogger;
-import org.entando.entando.ent.util.EntLogging.EntLogFactory;
 import org.entando.entando.plugins.jpseo.aps.system.services.page.PageMetatag;
 import org.entando.entando.plugins.jpseo.aps.system.services.page.SeoPageMetadata;
 import org.springframework.cache.Cache;
@@ -48,7 +48,7 @@ import org.springframework.cache.Cache;
  */
 public class SeoMappingCacheWrapper extends AbstractCacheWrapper implements ISeoMappingCacheWrapper {
 
-	private static final EntLogger _logger =  EntLogFactory.getSanitizedLogger(SeoMappingCacheWrapper.class);
+    private static final EntLogger _logger =  EntLogFactory.getSanitizedLogger(SeoMappingCacheWrapper.class);
 
     @Override
     public void release() {
@@ -59,48 +59,48 @@ public class SeoMappingCacheWrapper extends AbstractCacheWrapper implements ISeo
         cache.evict(DRAFT_PAGES_MAPPING);
     }
 
-	@Override
-	public void initCache(IPageManager pageManager, ISeoMappingDAO seoMappingDAO, boolean initDraftPageMapping) throws EntException {
-		try {
+    @Override
+    public void initCache(IPageManager pageManager, ISeoMappingDAO seoMappingDAO, boolean initDraftPageMapping) throws EntException {
+        try {
             Map<String, FriendlyCodeVO> mapping = seoMappingDAO.loadMapping();
-			Map<String, FriendlyCodeVO> pageFriendlyCodes = new HashMap<>();
-			Map<String, ContentFriendlyCode> contentFriendlyCodes = new HashMap<>();
-			Iterator<FriendlyCodeVO> codesIter = mapping.values().iterator();
-			while (codesIter.hasNext()) {
-				FriendlyCodeVO currentCode = codesIter.next();
-				if (currentCode.getPageCode()!=null) {
-					pageFriendlyCodes.put(currentCode.getPageCode(), currentCode);
-				} else if (currentCode.getContentId()!=null) {
-					String contentId = currentCode.getContentId();
-					ContentFriendlyCode content = contentFriendlyCodes.get(contentId);
-					if (content==null) {
-						content = new ContentFriendlyCode();
-						content.setContentId(contentId);
-						contentFriendlyCodes.put(contentId, content);
-					}
-					content.addFriendlyCode(currentCode.getLangCode(), currentCode.getFriendlyCode());
-				}
-			}
+            Map<String, FriendlyCodeVO> pageFriendlyCodes = new HashMap<>();
+            Map<String, ContentFriendlyCode> contentFriendlyCodes = new HashMap<>();
+            Iterator<FriendlyCodeVO> codesIter = mapping.values().iterator();
+            while (codesIter.hasNext()) {
+                FriendlyCodeVO currentCode = codesIter.next();
+                if (currentCode.getPageCode()!=null) {
+                    pageFriendlyCodes.put(currentCode.getPageCode(), currentCode);
+                } else if (currentCode.getContentId()!=null) {
+                    String contentId = currentCode.getContentId();
+                    ContentFriendlyCode content = contentFriendlyCodes.get(contentId);
+                    if (content==null) {
+                        content = new ContentFriendlyCode();
+                        content.setContentId(contentId);
+                        contentFriendlyCodes.put(contentId, content);
+                    }
+                    content.addFriendlyCode(currentCode.getLangCode(), currentCode.getFriendlyCode());
+                }
+            }
             Cache cache = this.getCache();
             this.insertAndCleanVoObjectsOnCache(cache, mapping, MAPPING_BY_CODE_CACHE_KEY, MAPPING_BY_CODE_CACHE_KEY_PREFIX);
             this.insertAndCleanVoObjectsOnCache(cache, pageFriendlyCodes, MAPPING_BY_PAGE_CACHE_KEY, MAPPING_BY_PAGE_CACHE_KEY_PREFIX);
-			this.insertAndCleanVoObjectsOnCache(cache, contentFriendlyCodes, MAPPING_BY_CONTENT_CACHE_KEY, MAPPING_BY_CONTENT_CACHE_KEY_PREFIX);
+            this.insertAndCleanVoObjectsOnCache(cache, contentFriendlyCodes, MAPPING_BY_CONTENT_CACHE_KEY, MAPPING_BY_CONTENT_CACHE_KEY_PREFIX);
             if (initDraftPageMapping) {
                 this.createDraftPagesMapping(pageManager, cache);
             }
-		} catch (Throwable t) {
-			_logger.error("Error loading seo mapper", t);
-			throw new EntException("Error loading seo mapper", t);
-		}
-	}
-    
+        } catch (Throwable t) {
+            _logger.error("Error loading seo mapper", t);
+            throw new EntException("Error loading seo mapper", t);
+        }
+    }
+
     protected Map<String, String> createDraftPagesMapping(IPageManager pageManager, Cache cache) {
         Map<String, String> mapping = new HashMap<>();
         this.createDraftPagesMapping(pageManager, pageManager.getDraftRoot(), mapping);
         cache.put(DRAFT_PAGES_MAPPING, mapping);
         return mapping;
     }
-    
+
     protected void createDraftPagesMapping(IPageManager pageManager, IPage current, Map<String, String> mapping) {
         if (null == current) {
             return;
@@ -118,56 +118,56 @@ public class SeoMappingCacheWrapper extends AbstractCacheWrapper implements ISeo
             }
         }
     }
-    
+
     protected void releaseCachedObjects(Cache cache, String listKey, String prefixKey) {
         IFCacheWithPipeline.pipelined(cache.getNativeCache(), cp -> {
-		List<String> codes = (List<String>) this.get(cache, listKey, List.class);
-		if (null != codes) {
-			for (String code : codes) {
-				cache.evict(prefixKey + code);
-			}
-			cache.evict(listKey);
-		}
-        });
-	}
-    
-    protected void insertAndCleanVoObjectsOnCache(Cache cache, Map<String, ?> objects, String listKey, String cacheKeyPrefix) {
-        IFCacheWithPipeline.pipelined(cache.getNativeCache(), cp -> {
-        List<String> oldCodes = (List<String>) this.get(cache, listKey, List.class);
-        List<String> oldCodesClone = (null != oldCodes) ? new ArrayList<>(oldCodes) : null;
-        List<String> codes = new ArrayList<>();
-        Iterator<String> iter = objects.keySet().iterator();
-        while (iter.hasNext()) {
-            String key = iter.next();
-            cache.put(cacheKeyPrefix + key, objects.get(key));
-            if (null != oldCodesClone) {
-                oldCodesClone.remove(key);
+            List<String> codes = (List<String>) this.get(cache, listKey, List.class);
+            if (null != codes) {
+                for (String code : codes) {
+                    cache.evict(prefixKey + code);
+                }
+                cache.evict(listKey);
             }
-            codes.add(key);
-        }
-        cache.put(listKey, codes);
-        if (null != oldCodesClone) {
-            for (String code : oldCodesClone) {
-                cache.evict(cacheKeyPrefix + code);
-            }
-        }
         });
     }
-    
-	@Override
-	public FriendlyCodeVO getMappingByFriendlyCode(String friendlyCode) {
+
+    protected void insertAndCleanVoObjectsOnCache(Cache cache, Map<String, ?> objects, String listKey, String cacheKeyPrefix) {
+        IFCacheWithPipeline.pipelined(cache.getNativeCache(), cp -> {
+            List<String> oldCodes = (List<String>) this.get(cache, listKey, List.class);
+            List<String> oldCodesClone = (null != oldCodes) ? new ArrayList<>(oldCodes) : null;
+            List<String> codes = new ArrayList<>();
+            Iterator<String> iter = objects.keySet().iterator();
+            while (iter.hasNext()) {
+                String key = iter.next();
+                cache.put(cacheKeyPrefix + key, objects.get(key));
+                if (null != oldCodesClone) {
+                    oldCodesClone.remove(key);
+                }
+                codes.add(key);
+            }
+            cache.put(listKey, codes);
+            if (null != oldCodesClone) {
+                for (String code : oldCodesClone) {
+                    cache.evict(cacheKeyPrefix + code);
+                }
+            }
+        });
+    }
+
+    @Override
+    public FriendlyCodeVO getMappingByFriendlyCode(String friendlyCode) {
         return this.get(this.getCache(), MAPPING_BY_CODE_CACHE_KEY_PREFIX + friendlyCode, FriendlyCodeVO.class);
-	}
+    }
 
     @Override
     public FriendlyCodeVO getMappingByPageCode(String pageCode) {
         return this.get(this.getCache(), MAPPING_BY_PAGE_CACHE_KEY_PREFIX+ pageCode, FriendlyCodeVO.class);
-	}
-    
+    }
+
     @Override
     public ContentFriendlyCode getMappingByContentId(String contentId) {
         return this.get(this.getCache(), MAPPING_BY_CONTENT_CACHE_KEY_PREFIX + contentId, ContentFriendlyCode.class);
-	}
+    }
 
     @Override
     public String getDraftPageReference(String friendlyCode) {
@@ -191,5 +191,5 @@ public class SeoMappingCacheWrapper extends AbstractCacheWrapper implements ISeo
     protected String getCacheName() {
         return ISeoMappingCacheWrapper.SEO_MAPPER_CACHE_NAME;
     }
-	
+
 }
